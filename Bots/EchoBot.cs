b70bdb32727452b2904a60605e8be04b0be78342
Aris <deloittecanada123@gmail.com>
2020-05-17 20:26:38 -0600

    Add project files.

---
+++ Bots/EchoBot.cs
@@ -0,0 +1,75 @@
+﻿// Copyright (c) Microsoft Corporation. All rights reserved.
+// Licensed under the MIT License.
+
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics;
+using Microsoft.Azure.CognitiveServices.Language.TextAnalytics.Models;
+using Microsoft.Bot.Builder;
+using Microsoft.Bot.Schema;
+
+using Microsoft.Rest;
+
+namespace Microsoft.BotBuilderSamples.Bots
+{
+    public class ApiKeyServiceClientCredentials : ServiceClientCredentials
+    {
+        private readonly string subscriptionKey;
+
+        /// <summary>
+        /// Creates a new instance of the ApiKeyServiceClientCredentails class
+        /// </summary>
+        /// <param name="subscriptionKey">The subscription key to authenticate and authorize as</param>
+        public ApiKeyServiceClientCredentials(string subscriptionKey)
+        {
+            this.subscriptionKey = subscriptionKey;
+        }
+
+        /// <summary>
+        /// Add the Basic Authentication Header to each outgoing request
+        /// </summary>
+        /// <param name="request">The outgoing request</param>
+        /// <param name="cancellationToken">A token to cancel the operation</param>
+        public override Task ProcessHttpRequestAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            if (request == null)
+            {
+                throw new ArgumentNullException("request");
+            }
+
+            request.Headers.Add("Ocp-Apim-Subscription-Key", this.subscriptionKey);
+
+            return base.ProcessHttpRequestAsync(request, cancellationToken);
+        }
+    }
+
+
+    public class EchoBot : ActivityHandler
+    {
+        protected override async Task OnMessageActivityAsync(ITurnContext<IMessageActivity> turnContext, CancellationToken cancellationToken)
+        {
+            string key = "c5350e440b0e4f61884b61e5786962cc";
+            string endpoint = "https://ai-meetup-entity-rec.cognitiveservices.azure.com/";
+
+            
+
+            var replyText = $"Your detected language is: unknown to me.";
+            await turnContext.SendActivityAsync(MessageFactory.Text(replyText, replyText), cancellationToken);
+        }
+
+        protected override async Task OnMembersAddedAsync(IList<ChannelAccount> membersAdded, ITurnContext<IConversationUpdateActivity> turnContext, CancellationToken cancellationToken)
+        {
+            var welcomeText = "Hello and welcome!";
+            foreach (var member in membersAdded)
+            {
+                if (member.Id != turnContext.Activity.Recipient.Id)
+                {
+                    await turnContext.SendActivityAsync(MessageFactory.Text(welcomeText, welcomeText), cancellationToken);
+                }
+            }
+        }
+    }
+}
